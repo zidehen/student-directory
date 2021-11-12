2523dbbb0329f6b62a8b080f8a6ea430e5f2161a
zidehen <zoeidehen@gmail.com>
2021-11-12 15:57:10 +0000

    Step 8 exercises

--- directory.rb
+++ directory.rb
@@ -4,36 +4,94 @@ def input_students
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  name = gets.delete!("\n")
   # while the name is not empty, repeat this code
   while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    puts "What cohort are they in?"
+    month = gets.delete!("\n")
+    #set a default month if no cohort entered
+      if month == ""
+        month = "november"
+      end
+    puts "What is their favourite hobby?"
+    hobby = gets.delete!("\n")
+    # add all student info as a hash into the array
+    students << {name: name, cohort: month.to_sym, hobbies: hobby}
+    if students.length == 1 
+      puts "Now we have #{students.count} student"
+    else
+      puts "Now we have #{students.count} students"
+    end
     # get another name from the user
+    puts "Next student?"
     name = gets.chomp
   end
   # return the array of students 
   students
 end
 
-def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
-end
-
-def print(students)
-  students.each do |student|
-  puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def print_header(students)
+  if !students.empty?
+    puts "The students of Villains Academy".center(40)
+    puts "-------------".center(40)
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print(students)
+  # set an accumulator so the loop can be broken
+  total_names = 0
+  # use until loop to print students details until total names matches total number of students
+  until total_names == students.count 
+    students.each.with_index(1) do |student, index|
+     puts "#{index}. #{student[:name]} --> cohort: #{student[:cohort]}, hobby: #{student[:hobbies]}".center(40)
+     total_names += 1
+    end
+  end
+end
+
+def print_first_letter(students)
+  puts "What letter would you like to search for?"
+  letter = gets.chomp
+  students.each do |student|
+    if student[:name][0] == letter
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+def print_names_less_than_12_chars(students)
+  students.each do |student|
+    if student[:name].length < 12
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
+def print_grouped_by_cohort(students)
+  puts "Select cohort group"
+  cohort = gets.chomp.to_sym
+  students.select do |student|
+    if student[:cohort] == cohort
+      puts "#{student[:name]} --> cohort: #{student[:cohort]}, hobby: #{student[:hobbies]}".center(40)
+    end
+  end
+end
+
+def print_footer(students)
+  if students.length == 0
+    puts "Overall, we have no students".center(40)
+  elsif students.length == 1
+    puts "Overall, we have #{students.count} great student".center(40)
+  else
+    puts "Overall, we have #{students.count} great students".center(40)
+  end
 end
 
 students = input_students
 # nothing happens until we call the methods
-print_header
+print_header(students)
 print(students)
+# print_first_letter(students)
+# print_names_less_than_12_chars(students)
+print_grouped_by_cohort(students)
 print_footer(students)
