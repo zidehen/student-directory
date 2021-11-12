1b5ff05184cec746e55ae1b159ae0d53f37fccd3
zidehen <zoeidehen@gmail.com>
2021-11-12 22:21:50 +0000

    Method added to save student data to file

--- directory.rb
+++ directory.rb
@@ -40,6 +40,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
@@ -57,6 +58,8 @@ def process(selection)
     when "2"
       show_students
       # show the students
+    when "3"
+      save_students
     when "9"
       exit # this will cause the program to terminate
     else 
@@ -120,6 +123,18 @@ def print_grouped_by_cohort
     end
   end
 end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end 
 
 interactive_menu
 # nothing happens until we call the methods
