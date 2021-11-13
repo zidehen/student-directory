c4ae49a5c6349d110b6cae95307380318dbac2ae
zidehen <zoeidehen@gmail.com>
2021-11-13 12:14:03 +0000

    Method added that uses command line arguments to load students data

--- directory.rb
+++ directory.rb
@@ -4,17 +4,17 @@
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first name
-  name = gets.delete!("\n")
+  name = STDIN.gets.delete!("\n")
   # while the name is not empty, repeat this code
   while !name.empty? do
     puts "What cohort are they in?"
-    month = gets.delete!("\n")
+    month = STDIN.gets.delete!("\n")
     #set a default month if no cohort entered
       if month == ""
         month = "november"
       end
     puts "What is their favourite hobby?"
-    hobby = gets.delete!("\n")
+    hobby = STDIN.gets.delete!("\n")
     # add all student info as a hash into the array
     @students << {name: name, cohort: month.to_sym, hobbies: hobby}
     if @students.length == 1 
@@ -24,16 +24,7 @@
     end
     # get another name from the user
     puts "Next student?"
-    name = gets.chomp
-  end
-  # return the array of students 
-  # students
-end
-
-def interactive_menu
-  loop do
-    print_menu # 1. print the menu and ask the user what to do
-    process(gets.chomp) # 2. read the input and save it into a variable 
+    name = STDIN.gets.chomp
   end
 end
 
@@ -45,9 +36,16 @@ def print_menu
   puts "9. Exit"
 end
 
+def interactive_menu
+  loop do
+    print_menu # 1. print the menu and ask the user what to do
+    process(STDIN.gets.chomp) # 2. read the input and save it into a variable 
+  end
+end
+
 def show_students
   print_header
-  print_students_list
+  print_student_list
   print_footer
 end
 
@@ -55,10 +53,8 @@ def process(selection)
   case selection
     when "1"
       input_students
-      # input the students
     when "2"
-      show_students
-      # show the students
+      show_students 
     when "3"
       save_students
     when "4"
@@ -77,7 +73,7 @@ def print_header
   end
 end
 
-def print_students_list
+def print_student_list
   # set an accumulator so the loop can be broken
   total_names = 0
   # use until loop to print students details until total names matches total number of students
@@ -139,8 +135,8 @@ def save_students
   file.close
 end 
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
   name, cohort, hobby = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym, hobbies: hobby}
@@ -148,6 +144,19 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exist?(filename) # if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
+end
+
+try_load_students
 interactive_menu
 # nothing happens until we call the methods
 # print_first_letter(students)
